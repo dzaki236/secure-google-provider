a12381644fda189e10a0cb6d83eeb32eef211ef3
dzaki236 <dzakiahnafz@gmail.com>
2025-05-10 20:12:44 +0700

    update : 1.4 beta, change docs and verify settings from services config ✏️, docs update soon*\

--- src/ServiceProviders/SecureGoogleProviderServiceProvider.php
+++ src/ServiceProviders/SecureGoogleProviderServiceProvider.php
@@ -10,17 +10,19 @@ class SecureGoogleProviderServiceProvider extends ServiceProvider
 {
     /**
      * Bootstraping any application services from service provider.
-     * @return void;
+     * @return void
      */
     public function boot(): void
     {
         Socialite::extend('secure-google', function ($app) {
             $config = $app['config']['services.google'];
+            $verifyConfig = $app['config']['services.verify'];
             return new SecuringGoogleProvider(
-                request: $app['request'],
-                clientId: $config['client_id'],
-                clientSecret: $config['client_secret'],
-                redirectUrl: $config['redirect'],
+                $app['request'],
+                $config['client_id'],
+                $config['client_secret'],
+                $config['redirect'],
+                $verifyConfig // Pass entire of verify config
             );
         });
     }
--- src/Socialite/SecuringGoogleProvider.php
+++ src/Socialite/SecuringGoogleProvider.php
@@ -8,16 +8,35 @@ use GuzzleHttp\Client;
 class SecuringGoogleProvider extends GoogleProvider
 {
     /**
+     * @var verifyConfig;
+     */
+    protected array $verifyConfig = array();
+    /**
+     * Invoke construction params.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param string $clientId
+     * @param string $clientSecret
+     * @param string $redirectUrl
+     * @param array $verifyConfig
+     */
+    public function __construct(\Illuminate\Http\Request $request, $clientId, $clientSecret, $redirectUrl, array $verifyConfig)
+    {
+        $this->verifyConfig = $verifyConfig;
+        parent::__construct($request, $clientId, $clientSecret, $redirectUrl);
+    }
+    /**
      * Invoke Http Client function from parent.
      *
-     * @return \GuzzleHttp\Client;
+     * @return \GuzzleHttp\Client
      */
     protected function getHttpClient(): Client
     {
-        return new Client(config: [
-            'verify' => __DIR__ . '/../../certs/cacert.pem',
-            // NOT RECOMMENDED WAY for production!
-            /* 'verify' => false,*/
+        $verify = !empty($this->verifyConfig['force'])
+            ? __DIR__ . '/../../certs/cacert.pem'
+            : false;
+        return new Client([
+            'verify' => $verify,
         ]);
     }
 }
